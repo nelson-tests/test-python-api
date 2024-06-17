0b37c1bf0575647dc03271ec85d69cb042f5c5d1
Nelson Nobre <technobre@gmail.com>
2024-06-17 23:45:27 +0100

    fix: version 1.2.2

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def get_data():
     response = {
         'message': 'Hello, Python API!',
         'rightNow': datetime.datetime.now(datetime.timezone.utc),
-        'version': '1.3.0'
+        'version': '1.2.2'
     }
     return jsonify(response)
 
